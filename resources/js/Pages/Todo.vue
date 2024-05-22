f4e6ebdaa88661ae296160006ba19641953cb182
Massan0917 <massan000917@gmail.com>
2024-05-22 21:53:28 +0900

    表から叩く機構追加

--- resources/js/Pages/Todo.vue
+++ resources/js/Pages/Todo.vue
@@ -2,9 +2,27 @@
   import { ref } from 'vue'
   import axios from 'axios';
 
+
+  const postData = ref({
+    "title" : "task2",
+    "content" : "content2",
+    "priority" : "高",
+    "progress" : "未着手",
+    "severity" : "中",
+    "deadline" : "2024-12-31",
+    "user_id" : "1"
+    });
+
   axios.get('api/get/1').then(response => {
     console.log(response.data);
   });
+
+  function onclick() {
+    axios.post('api/register', postData.value).then(response => {
+      console.log(response.data);
+    });
+    alert('Hello World');
+  }
 </script>
 
 <template>
@@ -32,6 +50,7 @@
             </tbody>
         </v-table>
     <p class="font-serif ...">The quick brown fox ...</p>
+    <button @click="onclick">Click me</button>
     </body>
     
 </template>
