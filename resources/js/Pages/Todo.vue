1df3debcc1a796ba5413efae17926f26b266b268
Massan0917 <massan000917@gmail.com>
2024-05-28 21:38:53 +0900

    todo表示を適用

--- resources/js/Pages/Todo.vue
+++ resources/js/Pages/Todo.vue
@@ -23,9 +23,14 @@
     "deadline" : "2024-12-31",
     });
 
-  axios.get('api/get/1').then(response => {
-    console.log(response.data);
-  });
+  const todos = ref([]);
+  getTodos();
+
+  function getTodos() {
+    axios.get('api/get/1').then(response => {
+      todos.value = response.data.data;
+    });
+  }
 
   function registTask() {
     axios.post('api/register', postData.value).then(response => {
@@ -54,29 +59,30 @@
         <v-table>
             <thead>
                 <tr  style="border-color: white; border-width: 2px;">
-                <th>State</th>
-                <th>City</th>
+                <th>タスク名</th>
+                <th>内容</th>
+                <th>優先度</th>
+                <th>重度</th>
+                <th>進捗</th>
+                <th>締め切り</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody v-for="(todo) in todos">
                 <tr>
-                <td>Indiana</td>
-                <td>Indianapolis</td>
-                </tr>
-                <tr>
-                <td>Ohio</td>
-                <td>Columbus</td>-
-                </tr>
-                <tr>
-                <td>Michigan</td>
-                <td>Detroit</td>
+                <td>{{ todo.title }}</td>
+                <td>{{ todo.content }}</td>
+                <td>{{ todo.priority }}</td>
+                <td>{{ todo.severity }}</td>
+                <td>{{ todo.progress }}</td>
+                <td>{{ todo.deadline }}</td>
                 </tr>
             </tbody>
         </v-table>
-    <p class="font-serif ...">The quick brown fox ...</p>
-    <button @click="registTask">タスク登録</button><br>
-    <button @click="deleteTask">タスク削除</button><br>
-    <button @click="editTask">タスク編集</button>
+      <p class="font-serif ...">The quick brown fox ...</p>
+
+      <button @click="registTask">タスク登録</button><br>
+      <button @click="deleteTask">タスク削除</button><br>
+      <button @click="editTask">タスク編集</button>
     </body>
     
 </template>
